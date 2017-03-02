abc0f30bfff269c3d10877c1b4268ade3f8ee3b8
Derek Boytim <boytim.2@gmail.com>
2017-03-01 21:16:54 -0500

    Refactored tests to use paramaeters.
    Parameters provides a cleaner result when a test fails as opposed to looping through an array as I previously did.
    Added additional test values to increase to 100% code coverage.

--- src/test/java/DereksPrimeNumberGeneratorTest.java
+++ src/test/java/DereksPrimeNumberGeneratorTest.java
@@ -1,6 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -9,11 +13,11 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * Created by derek on 3/1/17.
  */
+@RunWith(value = Parameterized.class)
 public class DereksPrimeNumberGeneratorTest {
     private static int startingValue = 7900;
     private static int endingValue = 7920;
     private static int[] validPrimes = {7901, 7907, 7919};
-    private static int[] invalidPrimes = {-2, 4, 10, 9, 15, 21};
 
     private PrimeNumberGenerator underTest;
 
@@ -30,15 +34,34 @@ public class DereksPrimeNumberGeneratorTest {
 
     @Test
     public void shouldReturnTrueWhenValueIsPrime() throws Exception {
-        for (int i : validPrimes) {
-            assertThat(underTest.isPrime(i), equalTo(true));
-        }
+        assertThat(underTest.isPrime(7901), equalTo(true));
     }
 
     @Test
     public void shouldReturnFalseWhenValueIsNotPrime() throws Exception {
-        for (int i : invalidPrimes) {
-            assertThat(underTest.isPrime(i), equalTo(false));
-        }
+        assertThat(underTest.isPrime(7900), equalTo(false));
+    }
+
+    @Parameterized.Parameter(value = 0)
+    public int value;
+    @Parameterized.Parameter(value = 1)
+    public boolean result;
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                {-1, false},
+                {0, false},
+                {1, true},
+                {2, true},
+                {3, true},
+                {4, false},
+                {5, true},
+                {9, false}
+        });
+    }
+
+    @Test
+    public void shouldReturnBooleanByParameters() throws Exception {
+        assertThat(underTest.isPrime(value), equalTo(result));
     }
 }
